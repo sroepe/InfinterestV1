aeb8125cae1f6c6922fc317fdc2c80dfb6731d5b
Sara Roepe <sroepe@gmail.com>
2019-04-21 13:27:52 -0700

    updated broker dash

---
+++ Migrations/20190419013337_sarasmigrations7th41819.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infinterest.Migrations
+{
+    public partial class sarasmigrations7th41819 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
