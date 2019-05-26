2c5b5f703b9d26e849f62da08b54346a6e6ffe32
Hailey June <hailey.june.b@gmail.com>
2019-05-26 16:04:55 -0700

    request status mirgation

---
+++ Migrations/20190526230418_requeststatus.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infinterest.Migrations
+{
+    public partial class requeststatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Confirmed",
+                table: "eventvendors");
+
+            migrationBuilder.DropColumn(
+                name: "Denied",
+                table: "eventvendors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RequestStatus",
+                table: "eventvendors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestStatus",
+                table: "eventvendors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Confirmed",
+                table: "eventvendors",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Denied",
+                table: "eventvendors",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
